c49422825cacae5cf724e3b2b0c1993cb8c0b4fb
gubela28 <lukagubelidze28@gmail.com>
2024-01-10 21:24:34 +0400

    outside click

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,10 +1,8 @@
-import { useContext } from "react";
-import { useState } from "react";
-import { cloneElement } from "react";
-import { createContext } from "react";
+import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -78,15 +76,17 @@ function Open({ children, opens: opensWindowName }) {
 
 function Window({ children, name }) {
   const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
+
         <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
