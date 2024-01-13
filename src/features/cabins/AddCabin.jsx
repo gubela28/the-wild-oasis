5798bcd9edb272e6962da3dbf14d43f4570c1ef6
gubela28 <lukagubelidze28@gmail.com>
2024-01-13 20:25:59 +0400

    confirm cabin deletion

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -7,19 +7,14 @@ import CabinTable from "./CabinTable";
 function AddCabin() {
   return (
     <Modal>
-      <Modal.Open opens="cabin-form">
-        <Button>Add new cabin</Button>
-      </Modal.Open>
-      <Modal.Window name="cabin-form">
-        <CreateCabinForm />
-      </Modal.Window>
-
-      <Modal.Open opens="table">
-        <Button>Show table</Button>
-      </Modal.Open>
-      <Modal.Window name="table">
-        <CabinTable />
-      </Modal.Window>
+      <div>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </div>
     </Modal>
   );
 }
